f5123f57bd392db05dff67dcb8c608c2acaee930
VaibhaV-R-N <vaibhavnayak2120@gmail.com>
2023-09-29 17:38:42 +0530

    sigh

--- middlewares.js
+++ middlewares.js
@@ -4,7 +4,7 @@ module.exports.asyncCatcher = (fn)=>{
     return function(req,res,next){
         fn(req,res,next).catch((e)=>{
             console.log(e);
-            next(new AppError(e.toString(),'500'))
+            next(new AppError('something went wrong','500'))
         })
     }
 }
